85c99b6b303b8e65d2beb9718ae731b700a7dc4c
Dylan Guidry <dylan.guidry@students.aie.edu.au>
2017-01-04 16:22:49 -0600

    comments and more comments

---
+++ TeachingDeezKidz/SerlizationTutorialRedo/DataSerialization .cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SerlizationTutorialRedo
+{
+    using System.IO;
+
+    public static class DataSerialization<T>
+    {
+        /// <summary>
+        /// We invoke this function when have the data we want to serialize ready. The arguments we pass in to 
+        /// the function call tell the application where to save the data which by default is the project 
+        /// directory two files deep in a folder called SavedFiles and inside that folder we will find a file 
+        /// with the name that mathces the data stored inside the string passed into the function call.
+        /// Once that file has been found we then take the data passed into the fucntion and write it to said file
+        /// </summary>
+        /// <param name="fileName">Name of the file we are trying to save our data to</param>
+        /// <param name="data">The data we are saving</param>
+        public static void Serialize(string fileName, T data)
+        {
+            //Creates a new XMLSeualizer object with an argument of the type of the data passed in
+            XmlSerializer serilaizer = new XmlSerializer(typeof(T));
+            //Creates a new TextWritter object and assigns it a StreamWritter object that takes in an 
+            //argument of the file path
+            TextWriter writter = new StreamWriter(@"..\..\SavedFiles\" + fileName + ".xml");
+            //Invoke the Serialze function from the XmlSerializer class and pass in the TextWritter object created
+            //and the data we are trying to write to the file
+            serilaizer.Serialize(writter, data);
+            //Closes the file we where using to write our data
+            writter.Close();
+        }
+
+
+        /// <summary>
+        /// We invoke this function when we are trying load somedata back into our application that has all ready been saved.
+        /// This function will break if you are trying to find a file that does not exist. We specify the file we are looking
+        /// for by the data stored in the string passed into the function call.
+        /// </summary>
+        /// <param name="fileName">Name of the file we are trying to access to get our data</param>
+        /// <returns></returns>
+        public static T Deserialize(string fileName)
+        {
+            //Creates a new Generic object we will use to hold the data we are trying to return
+            T data; 
+            //Creates a new XmlSerializer objects with an argument of the type of the generic object we are trying to create
+            XmlSerializer serilaizer = new XmlSerializer(typeof(T)); 
+            //Creates a new TextReader object with a child object of the class with the argument of a string that is the file
+            //directory of the file we are trying to get the data from
+            TextReader reader = new StreamReader(@"..\..\SavedFiles\" + fileName + ".xml");
+            //invokes the Deserialize function from the XmlSerializer class and casts the return as the generic type and
+            //and passess in the TextReader object as an arguement into the funciton call
+            data = (T)serilaizer.Deserialize(reader);
+            //Closes the TextReader object file
+            reader.Close();
+            //Returns our generic object
+            return data;
+        }
+    }
+}
--- TeachingDeezKidz/SerlizationTutorialRedo/Form1.cs
+++ TeachingDeezKidz/SerlizationTutorialRedo/Form1.cs
@@ -17,18 +17,37 @@ namespace SerlizationTutorialRedo
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Function that is invoked when the SaveData button is click by the user.
+        /// This function will create a new student with the information provided by the user from the various
+        /// fields on the winform.
+        /// Once we have the student created we will invoke the Serialize function from the DataSerialization class.
+        /// </summary>
+        /// <param name="sender">Object that invoked this method</param>
+        /// <param name="e">Arguments passed through by the object</param>
         private void SaveData_Click(object sender, EventArgs e)
         {
+            //Creates a student and invokes the constructor with the arguments being the data from the various fields
+            //in the winform
             Student curStudent = new Student(this.StudentName.Text, (int)this.StudentAge.Value, this.StudentID.Text);
-            DataSerlization<Student>.Serialize("Student", curStudent);
+            //Invokes the Serialize funciton from the DataSerialization class and gives it the arguements of the string
+            //"Student" and the newly created student
+            DataSerialization<Student>.Serialize("Student", curStudent);
         }
 
+        /// <summary>
+        /// Function that is invoked when he LoadLast button is clicked by the user.
+        /// This function will invoke the Deserialize function to create a new student and take that data and assign
+        /// the data from that student to the various fields.
+        /// </summary>
+        /// <param name="sender">Object that invoked this method</param>
+        /// <param name="e">Arguments passed through by the object</param>
         private void LoadLast_Click(object sender, EventArgs e)
         {
-            Student lastStudnet;
-            lastStudnet = DataSerlization<Student>.DeSeralize("Student");
-            this.StudentName.Text = lastStudnet.Name;
-            this.StudentAge.Value = lastStudnet.Age;
+            //Creates a new student with the information returned from the Deserialize function
+            Student lastStudnet = DataSerialization<Student>.Deserialize("Student"); 
+            this.StudentName.Text = lastStudnet.NAME;
+            this.StudentAge.Value = lastStudnet.AGE;
             this.StudentID.Text = lastStudnet.ID.ToString();
         }
     }
